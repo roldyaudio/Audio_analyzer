e25e8a356b744dd8c15920b5e7dcecb4bf576684
roldyaudio <roldyaudio@gmail.com>
2025-07-07 18:06:42 +0200

    Update lib_installer.py
    added warning if ffmpeg missing

--- lib_installer.py
+++ lib_installer.py
@@ -14,14 +14,24 @@ def ensure_pip():
         print("✅ pip installed successfully.")
 
 
+def check_ffmpeg_installed():
+    try:
+        subprocess.run(["ffmpeg", "-version"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    except FileNotFoundError:
+        return False
+
+
 def install_requirements_in_directory(base_dir):
     # Walk through all folders looking for requirements.txt files
     for root, dirs, files in os.walk(base_dir):
         for file in files:
             if file == "requirements.txt":
                 req_path = os.path.join(root, file)
-                print(f"\n🚀 Installing dependencies from: {req_path}")
-                print(f"📦 Running: {sys.executable} -m pip install -r {req_path}")
+                print(f"\n:rocket: Installing dependencies from: {req_path}")
+                print(f":package: Running: {sys.executable} -m pip install -r {req_path}")
 
                 # Run and show ALL output in real time
                 result = subprocess.run(
@@ -29,10 +39,14 @@ def install_requirements_in_directory(base_dir):
                 )
 
                 if result.returncode == 0:
-                    print(f"✅ Successfully installed from {req_path}")
+                    print(f":white_check_mark: Successfully installed from {req_path}")
                 else:
-                    print(f"❌ Error installing from {req_path}")
+                    print(f":x: Error installing from {req_path}")
                     sys.exit(1)
+
+    # Comprobar si FFmpeg está instalado al final
+    if not check_ffmpeg_installed():
+        print("Only FFmpeg is missing. Please install it to proceed.")
 
 
 if __name__ == "__main__":
