ba1b497b2f071c40ff7cc05877964f286d367331
roldyaudio <roldyaudio@gmail.com>
2025-07-10 17:49:43 +0200

    Update lib_installer.py

--- lib_installer.py
+++ lib_installer.py
@@ -46,7 +46,8 @@ def install_requirements_in_directory(base_dir):
 
     # Comprobar si FFmpeg está instalado al final
     if not check_ffmpeg_installed():
-        print("\033[91m Only FFmpeg is missing. Please install it to proceed. \033[0m")
+        print("\x1b[31mFFmpeg is not installed. Closing the application.\x1b[0m")
+        print("\x1b[33mRefer to 'Download' tab at the App Hub to install it.\x1b[0m")
 
 
 if __name__ == "__main__":
